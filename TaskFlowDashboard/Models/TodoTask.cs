95139c3af70ce4cbe42e7943e17108a4a6b43827
Iqra <247274@students.au.edu.pk>
2026-05-23 09:06:56 -0700

    Add project files.

---
+++ TaskFlowDashboard/Models/TodoTask.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskFlowDashboard.Models
+{
+    public class TodoTask
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ TaskFlowDashboard/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskFlowDashboard.Components;
+using TaskFlowDashboard.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// ✅ THIS IS REQUIRED
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
